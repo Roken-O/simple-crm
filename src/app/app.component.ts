1a5b1dc4f9faa532a1579cde8c868af9ebde665d
Roken Othman <roken.othman91@gmail.com>
2024-09-29 19:05:10 +0200

    routing to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
@@ -8,7 +8,7 @@ import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, CommonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, CommonModule, MatIconModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
